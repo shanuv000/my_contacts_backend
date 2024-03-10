2d34ac26a4464466396b104b5fe69b2731de905a
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2024-03-10 19:25:29 +0530

    ghcghcg

---
+++ config/dbConnection.js
@@ -0,0 +1,5 @@
+const Demo = () => {
+  console.log("B" + "a" + +"n" + "a");
+};
+module.exports = Demo;
+// https://youtu.be/H9M02of22z4?t=2441
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const Demo = require("./config/dbConnection");
 const donenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.use(errorHandler);
+Demo();
 app.listen(port, () => {
   console.log(`Server running on Port ${port}`);
 });
